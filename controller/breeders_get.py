27afa126a0385b2b52da36d56141b841f525c9ee
Matthias Tafelmeier <matthias.tafelmeier@gmx.net>
2025-05-27 20:43:18 +0200

    breeders list correct tsz normalizing

--- controller/breeders_get.py
+++ controller/breeders_get.py
@@ -3,13 +3,8 @@ from .archive_db import archive_db
 from .meta_data_db import queries
 from .meta_data_db import META_DB_CONFIG
 
-
-from .archive_db import archive_db
-from .meta_data_db import queries
-from .meta_data_db import META_DB_CONFIG
-
 import os
-
+from dateutil.parser import parse
 
 def main(request_data):
 
@@ -24,10 +19,9 @@ def main(request_data):
 
     if breeder_meta_data_list:
     # preformat timestamp to be stringifyable
-        configured_breeders = [(breeder_row[0],breeder_row[1].isoformat()) for breeder_row in breeder_meta_data_list]
+        configured_breeders = [(breeder_row[0],breeder_row[1], parse(str(breeder_row[2])).isoformat()) for breeder_row in breeder_meta_data_list]
 
     result_content = configured_breeders if configured_breeders else list()
 
     return { "result": "SUCCESS",
              "breeders": configured_breeders }
-
